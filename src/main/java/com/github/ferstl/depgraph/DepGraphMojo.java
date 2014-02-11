ade6b47bc0fd73610ddb936ff78dc3c9bfa2f560
Stefan Ferstl <st.ferstl@gmail.com>
2014-02-11 07:26:07 +0100

    Add non-aggregating graph mojo

---
+++ src/main/java/com/github/ferstl/depgraph/DepGraphMojo.java
@@ -0,0 +1,28 @@
+package com.github.ferstl.depgraph;
+
+import org.apache.maven.artifact.resolver.filter.ArtifactFilter;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.shared.dependency.graph.DependencyGraphBuilder;
+
+import com.github.ferstl.depgraph.dot.DotBuilder;
+
+@Mojo(
+    name = "graph",
+    aggregator = false,
+    defaultPhase = LifecyclePhase.NONE,
+    requiresDependencyCollection = ResolutionScope.TEST,
+    requiresDirectInvocation = true,
+    threadSafe = true)
+public class DepGraphMojo extends AbstractDepGraphMojo {
+
+  @Override
+  protected GraphFactory createGraphFactory(
+      DependencyGraphBuilder dependencyGraphBuilder, ArtifactFilter artifactFilter) {
+
+      DotBuilder dotBuilder = new DotBuilder(NodeRenderers.VERSIONLESS_ID, NodeRenderers.ARTIFACT_ID_LABEL);
+      return new AggregatingDotGraphFactory(dependencyGraphBuilder, artifactFilter, dotBuilder);
+  }
+
+}
