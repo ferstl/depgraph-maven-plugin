c3dd3b60cc1aa1bac5b6779b7a2fd4f94bff6eab
Stefan Ferstl <st.ferstl@gmail.com>
2014-02-24 23:41:22 +0100

    Add "tree" mojo

--- src/main/java/com/github/ferstl/depgraph/DepTreeMojo.java
+++ src/main/java/com/github/ferstl/depgraph/DepTreeMojo.java
@@ -8,18 +8,18 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
 import com.github.ferstl.depgraph.dot.DotBuilder;
 
 @Mojo(
-    name = "graph",
+    name = "tree",
     aggregator = false,
     defaultPhase = LifecyclePhase.NONE,
     requiresDependencyCollection = ResolutionScope.TEST,
     requiresDirectInvocation = true,
     threadSafe = true)
-public class DepGraphMojo extends AbstractDepGraphMojo {
+public class DepTreeMojo extends AbstractDepGraphMojo {
 
   @Override
   protected GraphFactory createGraphFactory(ArtifactFilter artifactFilter) {
-      DotBuilder dotBuilder = new DotBuilder(NodeRenderers.VERSIONLESS_ID, NodeRenderers.ARTIFACT_ID_LABEL);
-      return new SimpleGraphFactory(this.dependencyTreeBuilder, this.localRepository, artifactFilter, dotBuilder);
+    DotBuilder dotBuilder = new DotBuilder(NodeRenderers.VERSIONLESS_ID, NodeRenderers.ARTIFACT_ID_LABEL);
+    return new SimpleTreeGraphFactory(this.dependencyGraphBuilder, artifactFilter, dotBuilder);
   }
 
 }
