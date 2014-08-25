629be5a13b3343cf8264472661bdc541270e467d
Stefan Ferstl <st.ferstl@gmail.com>
2014-08-25 22:45:48 +0200

    More tests

--- src/test/java/com/github/ferstl/depgraph/dot/DotBuilderTest.java
+++ src/test/java/com/github/ferstl/depgraph/dot/DotBuilderTest.java
@@ -7,7 +7,11 @@ import org.junit.Test;
 
 import com.github.ferstl.depgraph.DependencyNodeAdapter;
 
-import static com.github.ferstl.depgraph.dot.DotBuilderMatcher.contains;
+import static com.github.ferstl.depgraph.dot.DotBuilderMatcher.emptyGraph;
+import static com.github.ferstl.depgraph.dot.DotBuilderMatcher.hasNodes;
+import static com.github.ferstl.depgraph.dot.DotBuilderMatcher.hasNodesAndEdges;
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.startsWith;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -23,13 +27,20 @@ public class DotBuilderTest {
   }
 
   @Test
+  public void graphStructure() {
+    String graph = this.dotBuilder.toString();
+    assertThat(graph, startsWith("digraph G {"));
+    assertThat(graph, endsWith("}"));
+  }
+
+  @Test
   public void defaults() {
     Node from = createNode("from");
     Node to = createNode("to");
 
     this.dotBuilder.addEdge(from, to);
 
-    assertThat(this.dotBuilder, contains(
+    assertThat(this.dotBuilder, hasNodesAndEdges(
         new String[] {
           "\"group:from:jar:1.0.0:compile\"[label=\"group:from:jar:1.0.0:compile\"]",
           "\"group:to:jar:1.0.0:compile\"[label=\"group:to:jar:1.0.0:compile\"]"},
@@ -38,6 +49,85 @@ public class DotBuilderTest {
 
   }
 
+  @Test
+  public void nullNodes() {
+    Node node = createNode("node");
+
+    this.dotBuilder.addEdge(node, null);
+    assertThat(this.dotBuilder, emptyGraph());
+
+    this.dotBuilder.addEdge(null, node);
+    assertThat(this.dotBuilder, emptyGraph());
+  }
+
+  @Test
+  public void omitSelfReferences() {
+    this.dotBuilder
+      .omitSelfReferences()
+      .addEdge(createNode("start"), createNode("start"));
+
+    assertThat(this.dotBuilder, hasNodes(
+        "\"group:start:jar:1.0.0:compile\"[label=\"group:start:jar:1.0.0:compile\"]"));
+
+    this.dotBuilder.addEdge(createNode("from"), createNode("to"));
+    assertThat(this.dotBuilder, hasNodesAndEdges(
+        new String[] {
+            "\"group:start:jar:1.0.0:compile\"[label=\"group:start:jar:1.0.0:compile\"]",
+            "\"group:from:jar:1.0.0:compile\"[label=\"group:from:jar:1.0.0:compile\"]",
+            "\"group:to:jar:1.0.0:compile\"[label=\"group:to:jar:1.0.0:compile\"]"},
+        new String[] {"\"group:from:jar:1.0.0:compile\" -> \"group:to:jar:1.0.0:compile\""}));
+  }
+
+
+  @Test
+  public void customNodeRenderer() {
+    this.dotBuilder.useNodeRenderer(TestRenderer.INSTANCE);
+
+    this.dotBuilder.addEdge(createNode("from"), createNode("to"));
+
+    assertThat(this.dotBuilder, hasNodesAndEdges(
+        new String[] {
+            "from[label=\"group:from:jar:1.0.0:compile\"]",
+            "to[label=\"group:to:jar:1.0.0:compile\"]"},
+        new String[] {"from -> to"}));
+  }
+
+  @Test
+  public void customEdgeRenderer() {
+    this.dotBuilder
+      .useEdgeRenderer(TestRenderer.INSTANCE)
+      .addEdge(createNode("from"), createNode("to"));
+
+    assertThat(this.dotBuilder, hasNodesAndEdges(
+        new String[] {
+            "\"group:from:jar:1.0.0:compile\"[label=\"group:from:jar:1.0.0:compile\"]",
+            "\"group:to:jar:1.0.0:compile\"[label=\"group:to:jar:1.0.0:compile\"]"},
+        new String[] {"\"group:from:jar:1.0.0:compile\" -> \"group:to:jar:1.0.0:compile\"[label=\"1.0.0\"]"}));
+  }
+
+  @Test
+  public void addEdgeWithCustomRenderer() {
+    this.dotBuilder.addEdge(createNode("from"), createNode("to"), TestRenderer.INSTANCE);
+
+    assertThat(this.dotBuilder, hasNodesAndEdges(
+        new String[] {
+            "\"group:from:jar:1.0.0:compile\"[label=\"group:from:jar:1.0.0:compile\"]",
+            "\"group:to:jar:1.0.0:compile\"[label=\"group:to:jar:1.0.0:compile\"]"},
+        new String[] {"\"group:from:jar:1.0.0:compile\" -> \"group:to:jar:1.0.0:compile\"[label=\"1.0.0\"]"}));
+
+  }
+
+  @Test
+  public void customNodeLabelRenderer() {
+    this.dotBuilder
+      .useNodeLabelRenderer(TestRenderer.INSTANCE)
+      .addEdge(createNode("from"), createNode("to"));
+
+    assertThat(this.dotBuilder, hasNodesAndEdges(new String[] {
+        "\"group:from:jar:1.0.0:compile\"[label=from]",
+        "\"group:to:jar:1.0.0:compile\"[label=to]"},
+    new String[] {"\"group:from:jar:1.0.0:compile\" -> \"group:to:jar:1.0.0:compile\""}));
+  }
 
 
   private Node createNode(String name) {
@@ -46,4 +136,20 @@ public class DotBuilderTest {
     return new DependencyNodeAdapter(artifact);
   }
 
+  static enum TestRenderer implements EdgeRenderer, NodeRenderer {
+    INSTANCE;
+
+    @Override
+    public String render(Node node) {
+      return node.getArtifact().getArtifactId();
+    }
+
+    @Override
+    public String createEdgeAttributes(Node from, Node to) {
+      return new AttributeBuilder()
+        .label(to.getArtifact().getVersion())
+        .toString();
+    }
+
+  }
 }
