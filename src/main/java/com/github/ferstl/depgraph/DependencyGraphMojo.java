f03ada030b276e6fdc00c1cee4753338aa02d812
Stefan Ferstl <st.ferstl@gmail.com>
2016-02-20 10:15:28 +0100

    Fix description of showVersions

--- src/main/java/com/github/ferstl/depgraph/DependencyGraphMojo.java
+++ src/main/java/com/github/ferstl/depgraph/DependencyGraphMojo.java
@@ -44,7 +44,7 @@ public class DependencyGraphMojo extends AbstractGraphMojo {
 
   /**
    * If set to {@code true}, the created graph will show version information an all artifacts. Depending on the flags
-   * {@link #showVersions} and {@link #showConflicts}, the version will either be shown directly in the artifact nodes
+   * {@link #showDuplicates} and {@link #showConflicts}, the version will either be shown directly in the artifact nodes
    * or on the dependency edges.
    *
    * @since 1.0.0
