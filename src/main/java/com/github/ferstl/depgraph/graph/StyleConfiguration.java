6ef027f7a418d772e3380b7a0ed8d6b388150d24
Stefan Ferstl <st.ferstl@gmail.com>
2016-06-06 23:31:55 +0200

    Simplify naming

--- src/main/java/com/github/ferstl/depgraph/graph/StyleConfiguration.java
+++ src/main/java/com/github/ferstl/depgraph/graph/StyleConfiguration.java
@@ -1,46 +1,14 @@
 package com.github.ferstl.depgraph.graph;
 
 import java.util.Map;
-import org.yaml.snakeyaml.DumperOptions;
-import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.introspector.BeanAccess;
 import com.google.common.collect.ImmutableMap;
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 public class StyleConfiguration {
 
-  NodeConfiguration defaultNodeConfiguration = new NodeConfiguration();
-  EdgeConfiguration defaultEdgeConfiguration = new EdgeConfiguration();
-  Map<String, NodeConfiguration> scopeNodeConfiguration = ImmutableMap.of("compile", new NodeConfiguration(), "test", new NodeConfiguration());
-  Map<NodeResolution, EdgeConfiguration> edgeTypeConfiguration = ImmutableMap.of(NodeResolution.INCLUDED, new EdgeConfiguration(), NodeResolution.OMITTED_FOR_DUPLICATE, new EdgeConfiguration());
-
-
-  public static void main(String[] args) {
-    createGson();
-    // createYaml();
-  }
-
-  private static void createGson() {
-    Gson gson = new GsonBuilder()
-        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_DASHES)
-        .setPrettyPrinting()
-        .create();
-
-    // NodeConfiguration[] c = new NodeConfiguration[]{new NodeConfiguration(), new NodeConfiguration(), new
-    // NodeConfiguration()};
-    // System.out.println(gson.toJson(c));
-    System.out.println(gson.toJson(new StyleConfiguration()));
-  }
-
-  private static void createYaml() {
-    DumperOptions options = new DumperOptions();
-
-    Yaml yaml = new Yaml(options);
-    yaml.setBeanAccess(BeanAccess.FIELD);
-    System.out.println(yaml.dumpAsMap(new StyleConfiguration()));
-  }
+  NodeConfiguration defaultNode = new NodeConfiguration();
+  EdgeConfiguration defaultEdge = new EdgeConfiguration();
+  Map<String, NodeConfiguration> scopedNodes = ImmutableMap.of("compile", new NodeConfiguration(), "test", new NodeConfiguration());
+  Map<NodeResolution, EdgeConfiguration> edgeTypes = ImmutableMap.of(NodeResolution.INCLUDED, new EdgeConfiguration(), NodeResolution.OMITTED_FOR_DUPLICATE, new EdgeConfiguration());
 
 
   static class NodeConfiguration {
