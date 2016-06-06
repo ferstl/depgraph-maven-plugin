2c26a364f60bd93f358cafa2d558e05d2a26cccf
Stefan Ferstl <st.ferstl@gmail.com>
2016-06-06 23:29:17 +0200

    Add edge configuration

--- src/main/java/com/github/ferstl/depgraph/graph/StyleConfiguration.java
+++ src/main/java/com/github/ferstl/depgraph/graph/StyleConfiguration.java
@@ -12,7 +12,36 @@ import com.google.gson.GsonBuilder;
 public class StyleConfiguration {
 
   NodeConfiguration defaultNodeConfiguration = new NodeConfiguration();
+  EdgeConfiguration defaultEdgeConfiguration = new EdgeConfiguration();
   Map<String, NodeConfiguration> scopeNodeConfiguration = ImmutableMap.of("compile", new NodeConfiguration(), "test", new NodeConfiguration());
+  Map<NodeResolution, EdgeConfiguration> edgeTypeConfiguration = ImmutableMap.of(NodeResolution.INCLUDED, new EdgeConfiguration(), NodeResolution.OMITTED_FOR_DUPLICATE, new EdgeConfiguration());
+
+
+  public static void main(String[] args) {
+    createGson();
+    // createYaml();
+  }
+
+  private static void createGson() {
+    Gson gson = new GsonBuilder()
+        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_DASHES)
+        .setPrettyPrinting()
+        .create();
+
+    // NodeConfiguration[] c = new NodeConfiguration[]{new NodeConfiguration(), new NodeConfiguration(), new
+    // NodeConfiguration()};
+    // System.out.println(gson.toJson(c));
+    System.out.println(gson.toJson(new StyleConfiguration()));
+  }
+
+  private static void createYaml() {
+    DumperOptions options = new DumperOptions();
+
+    Yaml yaml = new Yaml(options);
+    yaml.setBeanAccess(BeanAccess.FIELD);
+    System.out.println(yaml.dumpAsMap(new StyleConfiguration()));
+  }
+
 
   static class NodeConfiguration {
 
@@ -20,7 +49,13 @@ public class StyleConfiguration {
     int sides = 4;
     String color = "red";
     Font font = new Font();
+  }
 
+  static class EdgeConfiguration {
+
+    String style = "dotted";
+    String color = "black";
+    Font font = new Font();
   }
 
   static class Font {
