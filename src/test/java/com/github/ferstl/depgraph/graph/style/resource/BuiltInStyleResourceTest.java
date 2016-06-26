f10b651ab7b9a3599d320945f196b72bed32c3be
Stefan Ferstl <st.ferstl@gmail.com>
2016-06-26 21:39:04 +0200

    Convert to parameterized test

--- src/test/java/com/github/ferstl/depgraph/graph/style/resource/BuiltInStyleResourceTest.java
+++ src/test/java/com/github/ferstl/depgraph/graph/style/resource/BuiltInStyleResourceTest.java
@@ -1,20 +1,32 @@
 package com.github.ferstl.depgraph.graph.style.resource;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import com.github.ferstl.depgraph.graph.style.StyleConfiguration;
 import static org.junit.Assert.assertTrue;
 
-
+@RunWith(Parameterized.class)
 public class BuiltInStyleResourceTest {
+
+  private final BuiltInStyleResource resource;
+
+  public BuiltInStyleResourceTest(BuiltInStyleResource resource) {
+    this.resource = resource;
+  }
+
+  @Parameters(name = "{0}")
+  public static BuiltInStyleResource[] resources() {
+    return BuiltInStyleResource.values();
+  }
 
   @Test
   public void resourcesExistAndCanBeLoaded() {
-    for (BuiltInStyleResource resource : BuiltInStyleResource.values()) {
-      ClasspathStyleResource styleResource = resource.createStyleResource(getClass().getClassLoader());
+    ClasspathStyleResource styleResource = this.resource.createStyleResource(getClass().getClassLoader());
 
-      assertTrue("Style configuration " + resource + " does not exist.", styleResource.exists());
-      StyleConfiguration.load(styleResource);
-    }
+    assertTrue("Style configuration " + this.resource + " does not exist.", styleResource.exists());
+    StyleConfiguration.load(styleResource);
   }
 
 }
