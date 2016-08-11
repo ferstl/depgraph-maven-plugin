5a1a9b96540df2b646997a26429be9b8618f1431
Stefan Ferstl <st.ferstl@gmail.com>
2016-08-11 06:59:33 +0200

    Add simple integrationtest

---
+++ src/test/java/com/github/ferstl/depgraph/IntegrationTest.java
@@ -0,0 +1,36 @@
+package com.github.ferstl.depgraph;
+
+import java.io.File;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import io.takari.maven.testing.TestResources;
+import io.takari.maven.testing.executor.MavenExecutionResult;
+import io.takari.maven.testing.executor.MavenRuntime;
+import io.takari.maven.testing.executor.MavenRuntime.MavenRuntimeBuilder;
+import io.takari.maven.testing.executor.MavenVersions;
+import io.takari.maven.testing.executor.junit.MavenJUnitTestRunner;
+
+@RunWith(MavenJUnitTestRunner.class)
+@MavenVersions("3.3.9")
+public class IntegrationTest {
+
+  @Rule
+  public final TestResources resources = new TestResources();
+
+  private final MavenRuntime mavenRuntime;
+
+  public IntegrationTest(MavenRuntimeBuilder builder) throws Exception {
+    this.mavenRuntime = builder.build();
+  }
+
+  @Test
+  public void aggregate() throws Exception {
+    File basedir = this.resources.getBasedir("depgraph-maven-plugin-test");
+    MavenExecutionResult result = this.mavenRuntime
+        .forProject(basedir)
+        .execute("clean", "depgraph:aggregate");
+
+    result.assertErrorFreeLog();
+  }
+}
