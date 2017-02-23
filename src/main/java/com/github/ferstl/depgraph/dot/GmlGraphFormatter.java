806bfb3f14917928813b2c1d6a7e8ca80583d8be
Stefan Ferstl <st.ferstl@gmail.com>
2017-02-23 16:31:02 +0100

    #19: Adapt @pahan13 's .gml writing

---
+++ src/main/java/com/github/ferstl/depgraph/dot/GmlGraphFormatter.java
@@ -0,0 +1,31 @@
+package com.github.ferstl.depgraph.dot;
+
+import java.util.Collection;
+
+public class GmlGraphFormatter implements GraphFormatter {
+
+  @Override
+  public String format(String graphName, Collection<Node<?>> nodes, Collection<Edge> edges) {
+    StringBuilder result = new StringBuilder();
+    result.append("graph [\n");
+
+    //output nodes
+    for (Node<?> node : nodes) {
+      result.append("node [\n");
+      result.append("id \"").append(node.getNodeId()).append("\"\n");
+      result.append("label \"").append(node.getNodeName()).append("\"\n");
+      result.append("]\n\n");
+    }
+
+    //output edges
+    for (Edge edge : edges) {
+      result.append("edge [\n");
+      result.append("source \"").append(edge.getFromNodeId()).append("\"\n");
+      result.append("target \"").append(edge.getToNodeId()).append("\"\n");
+      result.append("]\n\n");
+    }
+
+    result.append("]");
+    return result.toString();
+  }
+}
