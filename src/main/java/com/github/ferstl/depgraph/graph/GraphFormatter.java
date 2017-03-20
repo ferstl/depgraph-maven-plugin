c653f75c9600f36131aa71d9783254701355b8d8
Stefan Ferstl <st.ferstl@gmail.com>
2017-03-20 22:30:54 +0100

    Doc

--- src/main/java/com/github/ferstl/depgraph/graph/GraphFormatter.java
+++ src/main/java/com/github/ferstl/depgraph/graph/GraphFormatter.java
@@ -2,6 +2,9 @@ package com.github.ferstl.depgraph.graph;
 
 import java.util.Collection;
 
+/**
+ * Format the given nodes and edges to a string representing the graph.
+ */
 public interface GraphFormatter {
 
   String format(String graphName, Collection<Node<?>> nodes, Collection<Edge> edges);
