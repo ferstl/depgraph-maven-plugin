3f7906eb8445c75bd2c0c4b5bc1c1e071b663a94
Stefan Ferstl <st.ferstl@gmail.com>
2017-04-23 08:49:38 +0200

    #30: Add label in addStyleAttributes() method

--- src/main/java/com/github/ferstl/depgraph/dependency/GmlDependencyEdgeRenderer.java
+++ src/main/java/com/github/ferstl/depgraph/dependency/GmlDependencyEdgeRenderer.java
@@ -18,20 +18,25 @@ public class GmlDependencyEdgeRenderer implements EdgeRenderer<DependencyNode> {
     StringBuilder builder = new StringBuilder();
 
     if (resolution == NodeResolution.OMITTED_FOR_CONFLICT) {
+      String version = "";
       if (this.renderVersion) {
-        builder.append("label \"").append(abbreviateVersion(to.getArtifact().getVersion())).append("\"");
+        version = abbreviateVersion(to.getArtifact().getVersion());
       }
 
-      addStyleAttributes(builder, "dashed", "#FF0000");
+      addStyleAttributes(builder, version, "dashed", "#FF0000");
 
     } else if (resolution == NodeResolution.OMITTED_FOR_DUPLICATE) {
-      addStyleAttributes(builder, "dotted", "");
+      addStyleAttributes(builder, "", "dotted", "");
     }
 
     return builder.toString();
   }
 
-  private static void addStyleAttributes(StringBuilder builder, String edgeStyle, String color) {
+  private static void addStyleAttributes(StringBuilder builder, String label, String edgeStyle, String color) {
+    if (!label.isEmpty()) {
+      builder.append("label \"").append(label).append("\"");
+    }
+
     builder.append("\n")
         .append("graphics\n")
         .append("[\n")
