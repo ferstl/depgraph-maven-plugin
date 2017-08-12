7c2beb2d67a9b4bc304dbe44fedec81200b18daf
Stefan Ferstl <st.ferstl@gmail.com>
2017-08-12 19:43:35 +0200

    #32 Make Artifact and Dependency inner classes of JsonGraph

--- src/main/java/com/github/ferstl/depgraph/graph/json/JsonGraph.java
+++ src/main/java/com/github/ferstl/depgraph/graph/json/JsonGraph.java
@@ -2,6 +2,7 @@ package com.github.ferstl.depgraph.graph.json;
 
 import java.util.ArrayList;
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonRawValue;
 
 class JsonGraph {
 
@@ -14,5 +15,40 @@ class JsonGraph {
 
   void addDependency(String fromNodeId, int fromNodeIdNumeric, String toNodeId, int toNodeIdNumeric, String data) {
     this.dependencies.add(new Dependency(fromNodeId, fromNodeIdNumeric, toNodeId, toNodeIdNumeric, data));
+  }
+
+  private static class Artifact {
+
+    private final String id;
+    private final int numericId;
+
+    @JsonRawValue
+    private final String data;
+
+    Artifact(String id, int numericId, String data) {
+      this.id = id;
+      this.numericId = numericId;
+      this.data = data;
+    }
+  }
+
+  private static class Dependency {
+
+    private final String from;
+    private final String to;
+    private final int numericFrom;
+    private final int numericTo;
+
+    @JsonRawValue
+    private final String data;
+
+    Dependency(String from, int numericFrom, String to, int numericTo, String data) {
+      this.from = from;
+      this.to = to;
+      this.numericFrom = numericFrom;
+      this.numericTo = numericTo;
+      this.data = data;
+    }
+
   }
 }
