ec86173e72461c4a0233e3745b97ca51899140ca
Stefan Ferstl <st.ferstl@gmail.com>
2017-08-19 19:20:41 +0200

    #46 Add test for 'mergeScopes' in the 'aggregate' goal

--- src/test/java/com/github/ferstl/depgraph/MergeOptionsIntegrationTest.java
+++ src/test/java/com/github/ferstl/depgraph/MergeOptionsIntegrationTest.java
@@ -64,4 +64,18 @@ public class MergeOptionsIntegrationTest {
 
     assertFileContents(basedir, "expectations/aggregate_no-merge.dot", "target/dependency-graph.dot");
   }
+
+  @Test
+  public void aggregateMergeScopes() throws Exception {
+    File basedir = this.resources.getBasedir("merge-test");
+    MavenExecutionResult result = this.mavenRuntime
+        .forProject(basedir)
+        .withCliOption("-DmergeScopes=true")
+        .execute("clean", "package", "depgraph:aggregate");
+
+    result.assertErrorFreeLog();
+    assertFilesPresent(basedir, "target/dependency-graph.dot");
+
+    assertFileContents(basedir, "expectations/aggregate_mergeScopes.dot", "target/dependency-graph.dot");
+  }
 }
