58f4e0bab2c6b2d77f00882a87a7c031db2c9562
Stefan Ferstl <st.ferstl@gmail.com>
2017-08-19 19:09:53 +0200

    #46 Add integration test for 'graph' goal where nothing is merged

---
+++ src/test/java/com/github/ferstl/depgraph/MergeOptionsIntegrationTest.java
@@ -0,0 +1,53 @@
+package com.github.ferstl.depgraph;
+
+import java.io.File;
+import java.nio.file.FileSystems;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import io.takari.maven.testing.TestResources;
+import io.takari.maven.testing.executor.MavenExecutionResult;
+import io.takari.maven.testing.executor.MavenRuntime;
+import io.takari.maven.testing.executor.MavenVersions;
+import io.takari.maven.testing.executor.junit.MavenJUnitTestRunner;
+
+import static io.takari.maven.testing.TestResources.assertFileContents;
+import static io.takari.maven.testing.TestResources.assertFilesPresent;
+
+@RunWith(MavenJUnitTestRunner.class)
+@MavenVersions({"3.5.0"})
+public class MergeOptionsIntegrationTest {
+
+  @Rule
+  public final TestResources resources = new TestResources();
+
+  private final MavenRuntime mavenRuntime;
+
+  public MergeOptionsIntegrationTest(MavenRuntime.MavenRuntimeBuilder builder) throws Exception {
+    this.mavenRuntime = builder.build();
+  }
+
+  @Before
+  public void before() {
+    // Workaround for https://github.com/takari/takari-plugin-testing-project/issues/14
+    FileSystems.getDefault();
+  }
+
+  @Test
+  public void graphNoMerge() throws Exception {
+    File basedir = this.resources.getBasedir("merge-test");
+    MavenExecutionResult result = this.mavenRuntime
+        .forProject(basedir)
+        .execute("clean", "package", "depgraph:graph");
+
+    result.assertErrorFreeLog();
+    assertFilesPresent(
+        basedir,
+        "module-1/target/dependency-graph.dot",
+        "module-2/target/dependency-graph.dot",
+        "target/dependency-graph.dot");
+
+    assertFileContents(basedir, "expectations/graph-module-1_no-merge.dot", "module-1/target/dependency-graph.dot");
+  }
+}
