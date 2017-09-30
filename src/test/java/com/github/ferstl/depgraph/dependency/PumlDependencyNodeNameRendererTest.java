77f0e45e9dc14fe611fc3915570de57e4034a970
Stefan Ferstl <st.ferstl@gmail.com>
2017-09-30 21:01:16 +0200

    #45 Make tests similar to the other NodeNameRenderers

--- src/test/java/com/github/ferstl/depgraph/dependency/PumlDependencyNodeNameRendererTest.java
+++ src/test/java/com/github/ferstl/depgraph/dependency/PumlDependencyNodeNameRendererTest.java
@@ -17,23 +17,202 @@ package com.github.ferstl.depgraph.dependency;
 
 import org.junit.Test;
 
+import static com.github.ferstl.depgraph.dependency.DependencyNodeUtil.addClassifiers;
+import static com.github.ferstl.depgraph.dependency.DependencyNodeUtil.addTypes;
+import static com.github.ferstl.depgraph.dependency.DependencyNodeUtil.createDependencyNode;
 import static org.junit.Assert.assertEquals;
 
 public class PumlDependencyNodeNameRendererTest {
 
+  @Test
+  public void renderNothing() {
+    // arrange
+    DependencyNode node = createDependencyNode("groupId", "artifactId", "version");
+    PumlDependencyNodeNameRenderer renderer = new PumlDependencyNodeNameRenderer(false, false, false, false, false);
 
-  private final PumlDependencyNodeNameRenderer renderer = new PumlDependencyNodeNameRenderer(true, true,false, false,  true);
+    // act
+    String result = renderer.render(node);
+
+    // assert
+    String expected = "{\"component\":\"rectangle\",\"label\":\"\",\"stereotype\":\"compile\"}";
+    assertEquals(expected, result);
+  }
 
   @Test
-  public void testRenderCompileNode() throws Exception {
+  public void renderGroupId() {
+    // arrange
+    DependencyNode node = createDependencyNode("groupId", "artifactId", "version");
+    PumlDependencyNodeNameRenderer renderer = new PumlDependencyNodeNameRenderer(true, false, false, false, false);
 
-    final DependencyNode dependencyNode = DependencyNodeUtil.createDependencyNode("org.springframework",
-        "spring-context", "4.3.9.RELEASE");
+    // act
+    String result = renderer.render(node);
 
-    String nodeInfo = this.renderer.render(dependencyNode);
+    // assert
+    String expected = "{\"component\":\"rectangle\",\"label\":\"groupId\",\"stereotype\":\"compile\"}";
+    assertEquals(expected, result);
+  }
 
-    assertEquals("{\"component\":\"rectangle\",\"label\":\"org.springframework:spring-context:4.3.9.RELEASE\",\"stereotype\":\"compile\"}",
-        nodeInfo);
+  @Test
+  public void renderArtifactId() {
+    // arrange
+    DependencyNode node = createDependencyNode("groupId", "artifactId", "version");
+    PumlDependencyNodeNameRenderer renderer = new PumlDependencyNodeNameRenderer(false, true, false, false,false);
 
+    // act
+    String result = renderer.render(node);
+
+    // assert
+    assertEquals("{\"component\":\"rectangle\",\"label\":\"artifactId\",\"stereotype\":\"compile\"}", result);
+  }
+
+  @Test
+  public void renderVersion() {
+    // arrange
+    DependencyNode node = createDependencyNode("groupId", "artifactId", "version");
+    PumlDependencyNodeNameRenderer renderer = new PumlDependencyNodeNameRenderer(false, false, false, false,true);
+
+    // act
+    String result = renderer.render(node);
+
+    // assert
+    String expected = "{\"component\":\"rectangle\",\"label\":\"version\",\"stereotype\":\"compile\"}";
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void renderGroupIdArtifactIdVersion() {
+    // arrange
+    DependencyNode node = createDependencyNode("groupId", "artifactId", "version");
+    PumlDependencyNodeNameRenderer renderer = new PumlDependencyNodeNameRenderer(true, true,false, false, true);
+
+    // act
+    String result = renderer.render(node);
+
+    // assert
+    String expected = "{\"component\":\"rectangle\",\"label\":\"groupId:artifactId:version\",\"stereotype\":\"compile\"}";
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void renderGroupIdArtifactId() {
+    // arrange
+    DependencyNode node = createDependencyNode("groupId", "artifactId", "version");
+    PumlDependencyNodeNameRenderer renderer = new PumlDependencyNodeNameRenderer(true, true, false, false,false);
+
+    // act
+    String result = renderer.render(node);
+
+    // assert
+    String expected = "{\"component\":\"rectangle\",\"label\":\"groupId:artifactId\",\"stereotype\":\"compile\"}";
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void renderGroupIdVersion() {
+    // arrange
+    DependencyNode node = createDependencyNode("groupId", "artifactId", "version");
+    PumlDependencyNodeNameRenderer renderer = new PumlDependencyNodeNameRenderer(true, false, false, false,true);
+
+    // act
+    String result = renderer.render(node);
+
+    // assert
+    String expected = "{\"component\":\"rectangle\",\"label\":\"groupId:version\",\"stereotype\":\"compile\"}";
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void renderArtifactIdVersion() {
+    // arrange
+    DependencyNode node = createDependencyNode("groupId", "artifactId", "version");
+    PumlDependencyNodeNameRenderer renderer = new PumlDependencyNodeNameRenderer(false, true,false, false, true);
+
+    // act
+    String result = renderer.render(node);
+
+    // assert
+    String expected = "{\"component\":\"rectangle\",\"label\":\"artifactId:version\",\"stereotype\":\"compile\"}";
+
+    assertEquals(expected, result);
+  }
+
+
+  @Test
+  public void renderTypes() {
+    // arrange
+    DependencyNode node = createDependencyNode("groupId", "artifactId", "version");
+    addTypes(node, "jar", "zip");
+    PumlDependencyNodeNameRenderer renderer = new PumlDependencyNodeNameRenderer(false, true, true, false, false);
+
+    // act
+    String result = renderer.render(node);
+
+    // assert
+    String expected = "{\"component\":\"rectangle\",\"label\":\"artifactId:jar/zip\",\"stereotype\":\"compile\"}";
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void renderJarTypeOnly() {
+    // arrange
+    DependencyNode node = createDependencyNode("groupId", "artifactId", "version");
+    addTypes(node, "jar");
+    PumlDependencyNodeNameRenderer renderer = new PumlDependencyNodeNameRenderer(false, true, true, false, false);
+
+    // act
+    String result = renderer.render(node);
+
+    // assert
+    String expected = "{\"component\":\"rectangle\",\"label\":\"artifactId:\",\"stereotype\":\"compile\"}";
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void renderClassifiers() {
+    // arrange
+    DependencyNode node = createDependencyNode("groupId", "artifactId", "version");
+    addClassifiers(node, "classifier1", "classifier2");
+    PumlDependencyNodeNameRenderer renderer = new PumlDependencyNodeNameRenderer(false, true, false, true, false);
+
+    // act
+    String result = renderer.render(node);
+
+    // assert
+    String expected = "{\"component\":\"rectangle\",\"label\":\"artifactId:classifier1/classifier2\",\"stereotype\":\"compile\"}";
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void renderEmptyClassifier() {
+    // arrange
+    DependencyNode node = createDependencyNode("groupId", "artifactId", "version");
+    addClassifiers(node, "");
+    PumlDependencyNodeNameRenderer renderer = new PumlDependencyNodeNameRenderer(false, true, false, true, false);
+
+    // act
+    String result = renderer.render(node);
+
+    // assert
+    String expected = "{\"component\":\"rectangle\",\"label\":\"artifactId:\",\"stereotype\":\"compile\"}";
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void renderAll() {
+    // arrange
+    DependencyNode node = createDependencyNode("groupId", "artifactId", "version", "test");
+    PumlDependencyNodeNameRenderer renderer = new PumlDependencyNodeNameRenderer(true, true, true, true, true);
+    addClassifiers(node, "classifier1", "classifier2");
+    addTypes(node, "jar", "zip", "tar.gz");
+
+    // act
+    String result = renderer.render(node);
+
+    // assert
+    String expected = "{\"component\":\"rectangle\",\"label\":\"groupId:artifactId:jar/tar.gz/zip:classifier1/classifier2:version\",\"stereotype\":\"test\"}";
+    assertEquals(expected, result);
   }
 }
