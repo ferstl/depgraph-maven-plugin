02abce08bd8101d772a01e8a57f31925d66d160c
Stefan Ferstl <st.ferstl@gmail.com>
2017-10-04 16:58:15 -0700

    #34 Add assertions

--- src/test/java/com/github/ferstl/depgraph/CollapseDependenciesIntegrationTest.java
+++ src/test/java/com/github/ferstl/depgraph/CollapseDependenciesIntegrationTest.java
@@ -12,6 +12,9 @@ import io.takari.maven.testing.executor.MavenRuntime;
 import io.takari.maven.testing.executor.MavenVersions;
 import io.takari.maven.testing.executor.junit.MavenJUnitTestRunner;
 
+import static io.takari.maven.testing.TestResources.assertFileContents;
+import static io.takari.maven.testing.TestResources.assertFilesPresent;
+
 @RunWith(MavenJUnitTestRunner.class)
 @MavenVersions({"3.5.0", "3.3.9"})
 public class CollapseDependenciesIntegrationTest {
@@ -32,13 +35,32 @@ public class CollapseDependenciesIntegrationTest {
   }
 
   @Test
-  public void aggregate() throws Exception {
+  public void aggregateWithReducedEdges() throws Exception {
     File basedir = this.resources.getBasedir("collapse-dependencies-test");
+    // Reduced edges are the default
     MavenExecutionResult result = this.mavenRuntime
         .forProject(basedir)
         .execute("clean", "package", "depgraph:aggregate");
 
     result.assertErrorFreeLog();
+    assertFilesPresent(
+        basedir, "target/dependency-graph.dot");
+
+    assertFileContents(basedir, "expectations/aggregated-reduced-edges.dot", "target/dependency-graph.dot");
   }
 
+  @Test
+  public void aggregateWithAllEdges() throws Exception {
+    File basedir = this.resources.getBasedir("collapse-dependencies-test");
+    MavenExecutionResult result = this.mavenRuntime
+        .forProject(basedir)
+        .withCliOption("-DreduceEdges=false")
+        .execute("clean", "package", "depgraph:aggregate");
+
+    result.assertErrorFreeLog();
+    assertFilesPresent(
+        basedir, "target/dependency-graph.dot");
+
+    assertFileContents(basedir, "expectations/aggregated-all-edges.dot", "target/dependency-graph.dot");
+  }
 }
