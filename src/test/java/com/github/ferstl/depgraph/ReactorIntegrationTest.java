0c8df0649050a4d394ec7a7ba9e61fedb9dc4424
Stefan Ferstl <st.ferstl@gmail.com>
2022-01-15 14:37:15 +0100

    #103 Implement build reactor graph

---
+++ src/test/java/com/github/ferstl/depgraph/ReactorIntegrationTest.java
@@ -0,0 +1,50 @@
+package com.github.ferstl.depgraph;
+
+import java.io.File;
+import java.nio.file.FileSystems;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import io.takari.maven.testing.TestResources;
+import io.takari.maven.testing.executor.MavenExecutionResult;
+import io.takari.maven.testing.executor.MavenRuntime;
+import io.takari.maven.testing.executor.MavenRuntime.MavenRuntimeBuilder;
+import io.takari.maven.testing.executor.MavenVersions;
+import io.takari.maven.testing.executor.junit.MavenJUnitTestRunner;
+import static com.github.ferstl.depgraph.MavenVersion.MAX_VERSION;
+import static com.github.ferstl.depgraph.MavenVersion.MIN_VERSION;
+
+@RunWith(MavenJUnitTestRunner.class)
+@MavenVersions({MIN_VERSION, MAX_VERSION})
+public class ReactorIntegrationTest {
+
+  @Rule
+  public final TestResources resources = new TestResources();
+
+  private final MavenRuntime mavenRuntime;
+
+  public ReactorIntegrationTest(MavenRuntimeBuilder builder) throws Exception {
+    this.mavenRuntime = builder
+        .withCliOptions("-B")
+        .build();
+  }
+
+  @Before
+  public void before() {
+    // Workaround for https://github.com/takari/takari-plugin-testing-project/issues/14
+    FileSystems.getDefault();
+  }
+
+  @Test
+  public void reactor() throws Exception {
+    File basedir = this.resources.getBasedir("reactor-graph");
+    MavenExecutionResult result = this.mavenRuntime
+        .forProject(basedir)
+        .withCliOption("-DgraphFormat=text")
+        .execute("clean", "depgraph:reactor");
+
+    result.assertErrorFreeLog();
+  }
+
+}
